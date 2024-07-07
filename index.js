5c7f7e8befb1783338f04af3323cdbeaaf563f9e
Sayad Bin Kamrul <74138515+ABID066@users.noreply.github.com>
2024-07-08 03:09:16 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-import { SpeedInsights } from "@vercel/speed-insights/react"
+//import { SpeedInsights } from "@vercel/speed-insights/react"
 
 app.listen(8080, function () {
     console.log("Server started on port 8080");
